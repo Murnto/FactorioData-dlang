002cbd081a877e2e151ed7dda0ba24705bc3f13a
David <Murnto@users.noreply.github.com>
2016-08-01 21:07:10 +1000

    Display git short hash in the footer

---
+++ source/layout_global.d
@@ -0,0 +1,34 @@
+import std.file;
+import std.path;
+import std.string;
+
+
+import std.stdio;
+
+string commitShort;
+string analytics = "";
+
+private string getCommitLong()
+{
+    immutable string headPath = ".git/HEAD";
+
+    if (isFile(headPath))
+    {
+        const string refHead = ".git/"~chomp(readText(headPath)).split(" ")[1];
+        
+        if (isFile(refHead))
+        {
+            return chomp(readText(refHead));
+        }
+    }
+    return null;
+}
+
+private string getCommitShort()
+{
+    return getCommitLong()[0..7];
+}
+
+static this() {
+    commitShort = getCommitShort();
+}
